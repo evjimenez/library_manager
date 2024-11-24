3130428b03d2ebe360cf9103e9d3b54c142d1fda
evjimenez <ejimenez.dev@gmail.com>
2024-11-24 09:10:32 -0600

    Modified config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
@@ -8,10 +9,14 @@ app = Flask(__name__)
 
 
 # Configuración de la base de datos
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'admin'
-app.config['MYSQL_DB'] = 'library_db'
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'admin'
+# app.config['MYSQL_DB'] = 'library_db'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'admin')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'library_db')
 
 mysql = MySQL(app)
 
